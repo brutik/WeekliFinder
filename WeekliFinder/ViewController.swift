3710d83e51e74396795d1636eb46e5976c4ea1ab
brutik <thoughtsoleg89@gmail.com>
2022-07-15 20:25:04 +0200

    First commit

---
+++ WeekliFinder/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  WeekliFinder
+//
+//  Created by Aleh Kuchynski on 03/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dateTF.text, let mounth = monthTF.text, let year = yearTF.text else {return}
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(mounth)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.locale = Locale(identifier: "pl_PL")
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekDay = dateFormatter.string(from: date)
+        let capitalizedWeekDay = weekDay.capitalized
+            resultLabel.text = capitalizedWeekDay
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
+
+
+
